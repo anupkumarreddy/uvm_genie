08e31062834bb9bd9dcaebe79d1b5a063f05554a
Anup Reddy <kumarreddy002@gmail.com>
2023-04-17 17:55:19 +0530

    updated config class with component iteration method

--- uvm_genie/config.py
+++ uvm_genie/config.py
@@ -1,29 +1,36 @@
 import yaml
 
+
 class UvmGenieConfig:
 
     def __init__(self):
         self.config = {
             'templates_path': 'templates',
             'output_path': 'output',
-            'components': [
-                {
-                    'driver': {
-                        'name': 'my_driver',
-                        'vif_name': 'my_vif'
-                    },
+            'components': {
+                'driver': {
+                    'filename': 'my_driver',
+                    'author': 'anup reddy',
+                    'class_name': 'FirstDriver',
+                    'description': "this is a simple test description for testing jinja template system",
+                    'template_name': 'driver.sv.jinja'
                 },
-                {
-                    'driver': {
-                        'name': 'my_driver',
-                        'vif_name': 'my_vif'
-                    }
+                'monitor': {
+                    'name': 'my_driver',
+                    'vif_name': 'my_vif'
                 }
-            ]
+
+            }
         }
+
+        self.component_iter = iter(self.config['components'])
 
     def load_config(self, configuration):
         self.__dict__.update(configuration)
+
+    def get_next_component(self):
+        component = next(self.component_iter)
+        return self.config['components'][component]
 
     def print_configuration(self):
         yaml_string = yaml.dump(self.config)
--- uvm_genie/uvm-genie.py
+++ uvm_genie/uvm-genie.py
@@ -1,5 +1,8 @@
 from uvm_genie.config import UvmGenieConfig
+from uvm_genie.generator import UvmGenerator
 
 
 configuration = UvmGenieConfig()
 configuration.print_configuration()
+generator = UvmGenerator()
+generator.generate(configuration)
