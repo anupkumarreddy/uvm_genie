282ef6e97efc0356488f78e9196aa5e6a0194fbd
anupkumarreddy <kumarreddy002@gmail.com>
2023-04-23 16:28:09 +0530

    Added argument parsing: Two command are allowed 1. generate : to generate all the components specified in default config for now (implement generation from user specified config later) 2. config : dump default config in to a user specified file or default file if user did not specify a file name

--- uvm_genie/uvm-genie.py
+++ uvm_genie/uvm-genie.py
@@ -1,8 +1,23 @@
 from uvm_genie.config import UvmGenieConfig
 from uvm_genie.generator import UvmGenerator
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("command", help="Command to perform, there are two commands 'config' and 'generate'",
+                    choices=['config', 'generate'])
+parser.add_argument("-v", "--verbose", action="store_true")
+parser.add_argument("-cf", "--config-file", help="Initial config is dumped with this file name")
+args = parser.parse_args()
 
 configuration = UvmGenieConfig()
-configuration.print_configuration()
+if args.command == 'generate':
+    print(args)
+elif args.command == 'config':
+    if args.config_file:
+        configuration.print_configuration_file(args.config_file)
+    else:
+        configuration.print_configuration_file('default.genie')
+
 generator = UvmGenerator()
 generator.generate(configuration)
