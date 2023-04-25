3e0cd8ed06b8afcff5a03bbe4840a308b08823de
anupkumarreddy <kumarreddy002@gmail.com>
2023-04-25 08:09:31 +0530

    Added --config-print option to print default configuration to the screen

--- uvm_genie/uvm-genie.py
+++ uvm_genie/uvm-genie.py
@@ -8,6 +8,8 @@ parser.add_argument("command", help="Command to perform, there are two commands 
                     choices=['config', 'generate'])
 parser.add_argument("-v", "--verbose", action="store_true")
 parser.add_argument("-cf", "--config-file", help="Initial config is dumped with this file name")
+parser.add_argument("-cp", "--config-print", action="store_true", help="Prints default configuration to screen")
+
 args = parser.parse_args()
 
 configuration = UvmGenieConfig()
@@ -18,6 +20,8 @@ elif args.command == 'config':
         configuration.print_configuration_file(args.config_file)
     else:
         configuration.print_configuration_file('default.genie')
+    if args.config_print:
+        print(configuration.print_configuration())
 
 generator = UvmGenerator()
 generator.generate(configuration)
